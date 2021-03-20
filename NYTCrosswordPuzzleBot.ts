a004e7503ccfd9ecf85b1ab846c7e19b9fc7c81b
Andrew Seale <awseale@email.arizona.edu>
2021-03-19 21:44:45 -0700

    got basic functionality

--- NYTCrosswordPuzzleBot.ts
+++ NYTCrosswordPuzzleBot.ts
@@ -1,27 +1,38 @@
 import * as dotenv from 'dotenv';
-import { Client } from 'discord.js';
+import { Client, User } from 'discord.js';
 import * as cron from 'node-cron';
 
 dotenv.config();
 
 const client = new Client();
 
-client.on('ready', () => {
+client.on('ready', async () => {
 	console.log("This boy done logged in");
+	
 
-	const channelName = client.channels.cache.get(process.env.CHANNEL);
+	const channelName = await client.channels.fetch(process.env.CHANNEL || "");
+
+	if(!channelName) {
+		console.log("no channel name found");	
+	}
 
 	//NYT Crossword updates at 10PM EST on weekdays and 6PM EST on weekends
 	//currently the bot will notify users at noon
+		
 	cron.schedule('* 19 * * Mon,Tue,Wed,Thur,Fri', () =>{
-		channelName.send('https://www.nytimes.com/crosswords/game/mini');
+		if(channelName){		
+			channelName.client.user?.send("Hey fellas! The NYT mini crossword just updated! Go get \'em!");
+			channelName.client.user?.send('https://www.nytimes.com/crosswords/game/mini');
+		}
 	});
 
-	cron.schedule('* 15 * * Sat, Sun', () =>{
-		channelName.send('Hey fellas! The NYT mini crossword just updated! Go get \'em!');
-		channelName.send('https://www.nytimes.com/crosswords/game/mini');
+	cron.schedule('* 15 * * Sat,Sun', () =>{
+		if(channelName){
+			channelName.client.user?.send('Hey fellas! The NYT mini crossword just updated! Go get \'em!');
+			channelName.client.user?.send('https://www.nytimes.com/crosswords/game/mini');
+		}
 	});
 
 });
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN || "");
