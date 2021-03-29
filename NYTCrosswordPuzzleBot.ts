2b4528fea4b7572e7f55e3ec5e1743bbbb75cfbf
Andrew Seale <awseale@email.arizona.edu>
2021-03-28 22:24:40 -0700

    added test log

--- NYTCrosswordPuzzleBot.ts
+++ NYTCrosswordPuzzleBot.ts
@@ -32,7 +32,7 @@ client.on('ready', async () => {
 			channelName.client.user?.send('https://www.nytimes.com/crosswords/game/mini');
 		}
 	});
-
+	console.log("This is a test");
 });
 
 client.login(process.env.TOKEN || "");
