ebab08b0b535f2f8bd10118f6786bb8f4369abf2
Keevan <keevandance@gmail.com>
2021-05-01 22:51:32 +0000

    Add tests for Cron class

--- src/Cron.ts
+++ src/Cron.ts
@@ -1,13 +1,16 @@
 import * as cron from 'node-cron';
 
+// https://help.nytimes.com/hc/en-us/articles/360052406391-The-New-York-Times-Crossword-puzzle#:~:text=Each%20daily%20crossword%20puzzle%20is,available%20at%2010%20p.m.%20E.S.T.
 export enum CRON_INTERVALS {
-    WEEKDAY = '0 0 2 * * Tuesday,Wednesday,Thursday,Friday,Saturday',
-    WEEKEND = '0 0 22 * * Saturday,Sunday'
+    WEEKDAY = "0 0 22 * * Monday,Tuesday,Wednesday,Thursday,Friday",
+    WEEKEND = "0 0 18 * * Saturday,Sunday",
 }
 
 class Cron {
     static startJob(interval: CRON_INTERVALS.WEEKDAY | CRON_INTERVALS.WEEKEND, callback: () => void) {
-        cron.schedule(interval, callback);
+        cron.schedule(interval, callback, {
+            timezone: "America/New_York"
+        });
     }
 }
 
