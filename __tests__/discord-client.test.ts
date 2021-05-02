52b825c9220b099742afba7897fecca181ff2ba2
Keevan <keevandance@gmail.com>
2021-05-02 01:09:54 +0000

    add discord client tests

---
+++ __tests__/discord-client.test.ts
@@ -0,0 +1,72 @@
+
+import DiscordClient, { MESSAGES } from "../src/DiscordClient"
+
+describe('DiscordClient', () => {
+
+    let discordClient: DiscordClient;
+    const sendMock = jest.fn()
+
+    beforeEach(() => {
+        jest.resetAllMocks()
+        discordClient = new DiscordClient()
+        // @ts-ignore
+        discordClient.client.channels.cache.get = jest.fn((channelName) => {
+            return {
+                send: sendMock
+            } 
+        })
+    })
+
+    describe('sendMessage()', () => {
+
+        test("successfully calls channel.send when called correctly", async () => {
+            await discordClient.sendMessage(MESSAGES.GOML, DiscordClient.CHANNELS.CHANGELOG)
+            expect(discordClient.client.channels.cache.get).toHaveBeenCalledWith(DiscordClient.CHANNELS.CHANGELOG)
+            expect(sendMock).toHaveBeenCalledWith(MESSAGES.GOML)
+        })
+
+        test("doesnt call channel.send when called with empty channel name", async () => {
+            await discordClient.sendMessage(MESSAGES.GOML, "")
+            expect(discordClient.client.channels.cache.get).not.toHaveBeenCalled()
+            expect(sendMock).not.toHaveBeenCalled()
+        })
+
+        test("doesnt call channel.send when it cannot find the channel from cache", async () => {
+            await discordClient.sendMessage(MESSAGES.GOML, "NON_EXISTING_CHANNEL")
+            expect(discordClient.client.channels.cache.get).not.toHaveBeenCalled()
+            expect(sendMock).not.toHaveBeenCalled()
+        })
+    })
+
+    describe('parseMessage()', () => {
+        
+        test("parses 'Normal message' correctly", () => {
+            const input = "Normal message"
+            const output = "normal message"
+            expect(discordClient.parseMessage(input)).toEqual(output)
+        })
+
+        test("parses '  NORMAL MESSAGE  ' correctly", () => {
+            const input = "  NORMAL MESSAGE  "
+            const output = "normal message"
+            expect(discordClient.parseMessage(input)).toEqual(output)
+        })
+
+    })
+
+    describe('isValidChannel', () => {
+
+        test("returns true if the channel is not empty", () => {
+            const input = "channel name"
+            const output = true
+            expect(discordClient.isValidChannel(input)).toEqual(output)
+        })
+
+        test("returns false when the channel is empty", () => {
+            const input = ""
+            const output = false
+            expect(discordClient.isValidChannel(input)).toEqual(output)
+        })
+    })
+
+})
--- src/DiscordClient.ts
+++ src/DiscordClient.ts
@@ -1,16 +1,16 @@
 import { Client, TextChannel } from 'discord.js';
 
+export enum MESSAGES {
+    UPDATE = "Ya boy is back and better than ever\nFor change notes, ask <@677740656747216916>",
+    PUZZLE = "Hey fellas! The NYT mini crossword just updated! Go get \'em!\nhttps://www.nytimes.com/crosswords/game/mini",
+    GOML = "GET ON HIS LEVEL"
+}
+
 class DiscordClient extends Client {
 	static CHANNELS = {
 		CHANGELOG: process.env.CHANGELOG_CHANNEL || "",
 		MAIN: process.env.CHANNEL || ""
 	}
-
-    static MESSAGES = {
-        UPDATE: "Ya boy is back and better than ever\nFor change notes, ask <@677740656747216916>",
-	    PUZZLE: "Hey fellas! The NYT mini crossword just updated! Go get \'em!\nhttps://www.nytimes.com/crosswords/game/mini",
-        GOML: "GET ON HIS LEVEL"
-    }
 
     client: Client;
 
@@ -19,15 +19,26 @@ class DiscordClient extends Client {
         this.client = new Client()
     }
 
-	async sendMessage(message: string, channelName: string) {
-        if (channelName === DiscordClient.CHANNELS.CHANGELOG || channelName === DiscordClient.CHANNELS.MAIN) {
-            const channel = await this.client.channels.cache.get(channelName) as TextChannel;
-            channel.send(message)
+	sendMessage = async (message: MESSAGES, channelName: string) => {
+        const hasValidChannels = this.isValidChannel(DiscordClient.CHANNELS.CHANGELOG) && this.isValidChannel(DiscordClient.CHANNELS.MAIN) 
+        const channelNameMatches = channelName === DiscordClient.CHANNELS.CHANGELOG || channelName === DiscordClient.CHANNELS.MAIN
+
+        if (hasValidChannels) {
+            if (channelNameMatches) {
+                const channel = await this.client.channels.cache.get(channelName);
+                (channel as TextChannel).send(message)
+            }
         }
 	}
 
-    parseMessage(message: string): string {
+    parseMessage = (message: string): string => {
         return message.toLowerCase().trim()
+    }
+
+    isValidChannel = (channelName: string): boolean => {
+        // since they are environment variables, we can only really check that
+        // they are not empty
+        return channelName.length > 0
     }
 }
 
