3d683a41641fba5cc3c2b82c22c8783ebdc05792
Andrew Seale <awseale@email.arizona.edu>
2021-05-02 00:25:00 -0700

    fixing Keevan's TRASH code

--- src/NYTCrosswordPuzzleBot.ts
+++ src/NYTCrosswordPuzzleBot.ts
@@ -1,36 +1,33 @@
 import * as dotenv from 'dotenv';
-import DiscordClient from "./DiscordClient"
+import DiscordClient, { MESSAGES } from "./DiscordClient"
 import Cron, { CRON_INTERVALS } from "./Cron"
 
 dotenv.config();
 
-
-const discordClient = new DiscordClient()
-
+const discordClient = new DiscordClient();
 
 discordClient.on('ready', async () => {
-	await discordClient.sendMessage(DiscordClient.MESSAGES.UPDATE, DiscordClient.CHANNELS.CHANGELOG)
+	await discordClient.sendMessage(DiscordClient.MESSAGES.UPDATE, DiscordClient.CHANNELS.CHANGELOG);
 	
 	//NYT Crossword updates at 10PM EST on weekdays and 6PM EST on weekends
 	Cron.startJob(CRON_INTERVALS.WEEKDAY, async () => {
-		await discordClient.sendMessage(DiscordClient.MESSAGES.PUZZLE, DiscordClient.CHANNELS.MAIN)
-	})
+		await discordClient.sendMessage(MESSAGES.PUZZLE, DiscordClient.CHANNELS.MAIN);
+	});
 
 	Cron.startJob(CRON_INTERVALS.WEEKEND, async () => {
-		await discordClient.sendMessage(DiscordClient.MESSAGES.PUZZLE, DiscordClient.CHANNELS.MAIN)
-	})
+		await discordClient.sendMessage(MESSAGES.PUZZLE, DiscordClient.CHANNELS.MAIN);
+	});
 });
 
 discordClient.on("message", async ({ content }) => {
-	const parsedMessage = discordClient.parseMessage(content)
+	const parsedMessage = discordClient.parseMessage(content);
 
 	switch (parsedMessage) {
 		case "goml":
 		case "#goml":
-			await discordClient.sendMessage(DiscordClient.MESSAGES.GOML, DiscordClient.CHANNELS.MAIN)
+			await discordClient.sendMessage(MESSAGES.GOML, DiscordClient.CHANNELS.MAIN);
 		default:
 	}
 })
 
 discordClient.login(process.env.TOKEN || "");
-
