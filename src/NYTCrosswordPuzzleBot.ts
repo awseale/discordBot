89b2cd91975be0711e502afd271067943a21ffd8
Andrew Seale <awseale@email.arizona.edu>
2021-05-02 15:45:03 -0700

    refactored NYTCrosswordPuzzleBot file to be more testable, and added tests

--- src/NYTCrosswordPuzzleBot.ts
+++ src/NYTCrosswordPuzzleBot.ts
@@ -8,23 +8,34 @@ const discordClient = new DiscordClient();
 discordClient.login(process.env.TOKEN || "");
 
 discordClient.on("ready", async () => {
-  console.log("ready");
-  await discordClient.sendMessage(MESSAGES.UPDATE, DiscordClient.CHANNELS.CHANGELOG);
-
-  //NYT Crossword updates at 10PM EST on weekdays and 6PM EST on weekends
-  Cron.startJob(CRON_INTERVALS.WEEKDAY, async () => {
-    await discordClient.sendMessage(MESSAGES.PUZZLE, DiscordClient.CHANNELS.MAIN);
-  });
-
-  Cron.startJob(CRON_INTERVALS.WEEKEND, async () => {
-    await discordClient.sendMessage(MESSAGES.PUZZLE, DiscordClient.CHANNELS.MAIN);
-  });
+	onReady(); 
 });
 
 discordClient.on("message", async ({ content }) => {
-  const parsedMessage = discordClient.parseMessage(content);
-
-  if (parsedMessage === "goml" || parsedMessage === "#goml") {
-    await discordClient.sendMessage(MESSAGES.GOML, DiscordClient.CHANNELS.MAIN);
-  }
+	gomlReply(content);
 });
+
+function onReady() {
+	console.log("ready");
+	await discordClient.sendMessage(MESSAGES.UPDATE, DiscordClient.CHANNELS.CHANGELOG);
+
+	Cron.startJob(CRON_INTERVALS.WEEKDAY, async () =>{
+		postCrossword();
+	});		
+
+	Cron.startJob(CRON_INTERVALS.WEEKEND, async () =>{
+		postCrossword();
+	});
+}
+
+function postCrossword() {
+	await discordClient.sendMessage(MESSAGES.PUZZLE, DiscordClient.CHANNELS.MAIN);
+}
+
+function gomlReply(content) {
+	const parsedMessage = discordClient.parseMessage(content);
+
+	if(parsedMessage === "goml" || parsedMessage === "#goml"){
+		await discordClient.sendMessage(MESSAGES.GOML, DiscordClient.CHANNELS.MAIN);
+	}
+}
